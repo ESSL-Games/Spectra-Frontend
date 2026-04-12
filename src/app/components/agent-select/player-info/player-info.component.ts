dddd98e585bf9d7d44b6b6b0c864277b20c95ae8
Paul Sommer <paul@sommers.dev>
2026-04-12 21:05:20 +0200

    Revert "feat: add name overwrite to agent select"
    This reverts commit 411e22151e3d9014efe401262e9e8500dfc09c74.

--- src/app/components/agent-select/player-info/player-info.component.ts
+++ src/app/components/agent-select/player-info/player-info.component.ts
@@ -1,6 +1,5 @@
-import { Component, effect, inject, Input, input } from "@angular/core";
+import { Component, effect, input } from "@angular/core";
 import { AgentRoleService } from "../../../services/agentRole.service";
-import { DisplayNameService } from "../../../services/displayName.service";
 
 @Component({
   selector: "app-agent-select-player-info",
@@ -9,14 +8,12 @@ import { DisplayNameService } from "../../../services/displayName.service";
   styleUrl: "./player-info.component.css",
 })
 export class AgentSelectPlayerInfoComponent {
-  @Input() player!: any;
   agent = input<string>("");
   locked = input<boolean>(false);
+  playerName = input<string>();
 
   color = input<string>();
   down = input<boolean>(false);
-
-  getDisplayName = inject(DisplayNameService).getDisplayName;
 
   previousAgent = "";
   previousLock = false;
